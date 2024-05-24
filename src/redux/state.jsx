0459d41d730100514833218616b6f9483f8257ec
akashsarker012 <akashsarker.dev@gmail.com>
2024-05-24 20:36:04 +0600

    Navbar in mui & logout using redux

--- src/redux/state.jsx
+++ src/redux/state.jsx
@@ -13,10 +13,15 @@ export const userSlice = createSlice({
       state.user = action.payload.user;
       state.token = action.payload.token;
     },
+    
+    setLogout: (state) => {
+      state.user = null;
+      state.token = null;
+    },
   },
 });
 
 // **Explicitly export setLogin action for clarity:**
-export const { login } = userSlice.actions;
+export const { login,setLogout } = userSlice.actions;
 
 export default userSlice.reducer;
